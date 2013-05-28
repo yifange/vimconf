adbe3ebbb72543a6cb6c230e12c926a1fbc69b4a
luzerno <irfan.1989@gmail.com>
2013-05-28 00:39:43 -0400

    almost done

---
+++ collect.sh
@@ -0,0 +1,4 @@
+cp ~/.vimrc .
+cp ~/.vim/bundles .vim/
+cp ~/.vim/tweaks.vim .vim/
+cp -r ~/.vim/vimrcs/ .vim/
---
+++ distribute.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.vim/temp_dirs
+cp .vimrc ~
+cp .vim/bundles ~/.vim/
+cp .vim/tweaks.vim ~/.vim/
+cp -r .vim/vimrcs/ ~/.vim/
